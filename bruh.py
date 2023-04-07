5cc8fa9ef78cafd6a70dbe158f2fc6ee098ab9b1
Dacyha <xd>
2023-04-08 01:07:42 +0300

    ia xz

--- bruh.py
+++ bruh.py
@@ -3,7 +3,6 @@ from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
 import numpy as np
-import numpy as np
 import io
 import streamlit as st
 from PIL import Image
